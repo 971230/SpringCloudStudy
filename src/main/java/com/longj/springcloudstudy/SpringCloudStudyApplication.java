34e8aff0cff8e12f2be896a6988d6de692620112
allanlong <1014093588@qq.com>
2022-07-11 21:07:46 +0800

    first commit

---
+++ src/main/java/com/longj/springcloudstudy/SpringCloudStudyApplication.java
@@ -0,0 +1,16 @@
+package com.longj.springcloudstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author long
+ */
+@SpringBootApplication
+public class SpringCloudStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudStudyApplication.class, args);
+    }
+
+}
